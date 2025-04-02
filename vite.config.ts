2124e6b36a2fa6dbc8fecd4ca728bbed2cdbb288
MSkrobot <Michal.skrobot@student.uj.edu.pl>
2025-04-02 21:28:31 +0200

    Fix: dodano base i poprawiono konfiguracj Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // to jest kluczowe!
   plugins: [react()],
+  server: {
+    host: true
+  }
 })
-server: {
-  host: true;
-}
